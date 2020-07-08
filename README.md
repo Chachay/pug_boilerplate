f2eea12da0a504b7984475f9453071c333b8928b
Chachay <t.sasai0802@gmail.com>
2020-07-08 16:57:39 +0200

    Add simple templates

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pug_bilerplate
+# pug_boilerplate
 My Boilerplate of Single HTML reports
+
+## Usage
+### Initialization
+
+```
+npm install --global gulp-cli
+npm init
+```
+
+### Build your report
+Put your .pug files in the src directory and type the following command.
+
+```
+gulp pug
+```
+
+Then, you will find html files under dest directory.
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const inlineSource = require('gulp-inline-source')
+const inlineImages = require('gulp-inline-images')
+
+gulp.task('pug', () => {
+  return gulp.src(['./src/*.pug', './src/**/*.pug', '!./_**/*', '!./node_modules/**/*'])
+    .pipe(pug({ pretty: true }))
+    .pipe(inlineSource())
+    .pipe(inlineImages({ basedir: './img/' }))
+    .pipe(gulp.dest('./dest/'))
+})
+gulp.task('watch', () => {
+  return gulp.watch(['./src/*.pug', './_template/*.pug', './_private/*.pug'], gulp.series(['pug']))
+})
+gulp.task('default', () => {
+  return gulp.parallel(['pug'])
+})
